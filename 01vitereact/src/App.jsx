32beab64ea527209fc332c2dad6dbfc6f76c94a1
harshalsonar5057 <harshal.sonar.5057@gmail.com>
2023-10-26 17:39:54 +0530

    Made three folder

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+import Chai from "./Chai";
+
+function App() {
+  // return <h1>Hello React from Vite</h1>;
+  return (
+  <><Chai /><h1>Hello React from Vite</h1></>
+  );
+}
+
+export default App;
